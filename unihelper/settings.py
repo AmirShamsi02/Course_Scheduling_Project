b9084ee941696d2e934ff4a6f500a9953d7232fb
Amirhossein Abolhasani <Amirhossein.abolhasani81.z@gmail.com>
2023-01-25 23:23:01 +0330

    secret key stays secret

--- unihelper/settings.py
+++ unihelper/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%f5k&c)lwj(2283@c8vp$39sf9*nnq@pb+2p1vej^r%zuk7o_'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
